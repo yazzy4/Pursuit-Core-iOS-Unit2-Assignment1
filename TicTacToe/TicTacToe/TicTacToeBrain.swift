46e84eb6c9fcdcd231b889d54e01f0ac8ffbdf12
yazzy4 <yazzy4@gmail.com>
2018-11-08 17:35:22 -0500

    first commit

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -9,5 +9,6 @@
 import Foundation
 
 class TicTacToeBrain {
+    //THIS IS WHERE YOU WILL MODEL YOUR LOGIC
   
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet weak var firstButton: UIButton!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        print("tic tac toe bruh")
+   
   }
+
 
 
 }
